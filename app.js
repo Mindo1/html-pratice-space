b08bfda776f43c9f7ecc41833b551e64b6a68104
Mindo1 <mind@odds.team>
2022-01-26 20:38:01 +0700

    🔥 js loop test

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 for (var counter = 0; counter<5;counter++){
     console.log("Hello"+counter)
     console.log("Hello World"+counter)
 }
 
 console.log("Exit")
-=======
-let lotto_number = prompt("กรุณากรอกหมายเลขที่ต้องการ")
-
-document.getElementById("s1").innerText = lotto_number
-document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
->>>>>>> d24e5b92e170ba1957e2d2ab64c6855cd9c187d5
